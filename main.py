21674889777e351923280b2d7bbea368be0eee00
MapleCCC <littlelittlemaple@gmail.com>
2019-11-09 13:45:21 +0800

    Add argument parsing logic, and implement neat debug mode setting

--- main.py
+++ main.py
@@ -7,11 +7,33 @@
 
 __author__ = 'MapleCCC, <littlelittlemaple@gmail.com>'
 
+import argparse
+import sys
+
 from utils import *
 
 
+def initialize_argparse(parser: argparse.ArgumentParser)->None:
+    parser.add_argument(
+        '-d',
+        '--debug',
+        dest='enabled_debug_mode',
+        action='store_true',
+        default=False,
+        help='Enabled debug mode')
+
+
 def main():
-    pass
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    initialize_argparse(parser)
+    args = parser.parse_args()
+
+    if not args.enabled_debug_mode:
+        # this clean trick also has desirable side effect that exception
+        # chaining is suppressed.
+        sys.excepthook = lambda exctype, exc, traceback: print(
+            f"{exctype.__name__}: {exc}")
 
 
 if __name__ == '__main__':
